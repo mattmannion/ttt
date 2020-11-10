dadcf9e39e10c25827df5cb4cfe040f82fb3dcc6
Matt Mannion <mattpmannion@gmail.com>
2020-11-10 18:20:50 -0500

    "updated js"

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /*
-  WELCOME TO TIC-TAC-TOE
+  WELCOME TO TIC-TAC-TOE // PROMPT-CONSOLE EDITION
 */
-let currentTurn = 0,
-  answer;
+
+// Data
 
 const player = {
   x: 'X',
-  y: 'Y',
+  o: 'O',
   e: '_',
   timer: 0, //ms
 };
@@ -17,22 +17,16 @@ let board = {
   c: ['_', '_', '_'],
 };
 
-function gameStart() {
-  for (let i = 0; i < board.a.length; i++) {
-    board.a[i] = player.e;
-    board.b[i] = player.e;
-    board.c[i] = player.e;
-  }
+const x = '',
+  o = '';
 
-  console.log(`
-   :1-2-3:
- a: ${board.a}
- b: ${board.b}
- c: ${board.c}
-  `);
-}
+let setPlayer;
+let currentTurn = 0;
+let answers = []; //[0] = player, [1] = position
 
-function setBoard(player, _) {
+//Function Definitions
+
+function setBoard(player) {
   board.a[0] = player;
   board.a[1] = player;
   board.a[2] = player;
@@ -45,7 +39,7 @@ function setBoard(player, _) {
   `);
 }
 
-function result(a, b, c) {
+function result(a, b) {
   if (
     //left to right [top]
     (a[0] === player.x && a[1] === player.x && a[2] === player.x) ||
@@ -64,29 +58,32 @@ function result(a, b, c) {
     //top to bottom [right]main
     (a[2] === player.x && b[2] === player.x && c[2] === player.x)
     //end
-  )
+  ) {
     console.log(`x wins`);
-  else if (
+    return player.x, false;
+  } else if (
     //left to right [top]
-    (a[0] === player.y && a[1] === player.y && a[2] === player.y) ||
+    (a[0] === player.o && a[1] === player.o && a[2] === player.o) ||
     //left to right [top]
-    (b[0] === player.y && b[1] === player.y && b[2] === player.y) ||
+    (b[0] === player.o && b[1] === player.o && b[2] === player.o) ||
     //left to right [top]
-    (b[0] === player.y && b[1] === player.y && b[2] === player.y) ||
+    (b[0] === player.o && b[1] === player.o && b[2] === player.o) ||
     //top left to bottom right [diagonal]
-    (a[0] === player.y && b[1] === player.y && c[2] === player.y) ||
+    (a[0] === player.o && b[1] === player.o && c[2] === player.o) ||
     //bottom left to top right [diagonal]
-    (a[2] === player.y && b[1] === player.y && c[0] === player.y) ||
+    (a[2] === player.o && b[1] === player.o && c[0] === player.o) ||
     //top to bottom [left]
-    (a[0] === player.y && b[0] === player.y && c[0] === player.y) ||
+    (a[0] === player.o && b[0] === player.o && c[0] === player.o) ||
     //top to bottom [middle]
-    (a[1] === player.y && b[1] === player.y && c[1] === player.y) ||
+    (a[1] === player.o && b[1] === player.o && c[1] === player.o) ||
     //top to bottom [right]
-    (a[2] === player.y && b[2] === player.y && c[2] === player.y)
+    (a[2] === player.o && b[2] === player.o && c[2] === player.o)
     //end
-  )
+  ) {
     console.log(`y wins`);
-  else console.log(`tie`);
+    return player.o, false;
+  } else console.log(`tie`);
+  return false;
 }
 
 function gameLoop() {
@@ -94,10 +91,45 @@ function gameLoop() {
   setBoard(player.e);
 }
 
-function main() {
-  answer = prompt('Enter either x or y');
+function input() {
+  answer[0] = prompt('Enter either x or y');
+  answer[1] = prompt('Now enter the desired free space');
   console.log(answer);
+  return answers;
 }
 
-gameStart();
+function turnMove(input) {
+  if (input[0] === player.x) {
+    currentTurn = player.o;
+  } else if (input[0] === player.o) {
+    currentTurn = player.x;
+  } else {
+    console.log('please enter X or Y');
+  }
+}
+
+// Function Calls
+
+//Set empty board and Initialize X or O
+(function () {
+  setPlayer = Math.trunc(Math.random() * 2);
+
+  if (setPlayer === 1) currentTurn = player.x;
+  else currentTurn = player.o;
+
+  for (let i = 0; i < board.a.length; i++) {
+    board.a[i] = player.e;
+    board.b[i] = player.e;
+    board.c[i] = player.e;
+  }
+
+  console.log(`
+   :1-2-3:
+ a: ${board.a}
+ b: ${board.b}
+ c: ${board.c}
+  `);
+})();
+
+console.log(setPlayer, currentTurn);
 // gameLoop();
